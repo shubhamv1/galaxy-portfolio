b7b74e37e1720e48045a54bc54fd6f92c2b5e2d8
SHUBHAM VERMA <66116917+shubhamv1@users.noreply.github.com>
2024-07-13 11:32:37 +0530

    Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Shubham`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>"Hello, I'm a dedicated Fullstack Developer Trainee on a journey to master the art of web development. My passion for coding started young, and I've been steadily building my skills in both front-end and back-end technologies.</p>
+                  <p>"Hello, I'm React JS Developer@QuadbTech.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
